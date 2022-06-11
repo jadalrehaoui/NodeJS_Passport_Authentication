6a249b96aed857cf2b052f85fc7b654ed31d0a0c
jadalrehaoui <jrehaoui.dev@outlook.com>
2022-06-11 16:10:12 -0600

    initial commit

--- config/keys.js
+++ config/keys.js
@@ -1,16 +1,6 @@
 module.exports = {
-  GOOGLE_ClientID: "820145471886-kmgp8bne0o8bttid7b3bk02ivqtp6u9i.apps.googleusercontent.com",
-  GOOGLE_ClientSecret: "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",
-  // MONGO_DB_PASSWORD: "3U52lsEXVA9HtVtk",
-  MONGO_DB_URI: "mongodb+srv://jadrehaoui:3U52lsEXVA9HtVtk@cluster0.vfabv.mongodb.net/?retryWrites=true&w=majority",
+  GOOGLE_ClientID: "",
+  GOOGLE_ClientSecret: "",
+  MONGO_DB_URI: "",
   COOKIE_KEY: "this-must-be-changed-before-production"
 }
-
-
-
-
-
-  // GOOGLE_ClientID: "",
-  // GOOGLE_ClientSecret: "",
-  // MONGO_DB_URI: "",
-  // COOKIE_KEY: "this-must-be-changed-before-production"
